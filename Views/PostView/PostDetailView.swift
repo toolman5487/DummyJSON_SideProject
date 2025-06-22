7b4ceca617d0f8565cdb075c4d5180a861689722
ToolManHsu <willy548798@gmail.com>
2025-06-22 18:07:45 +0800

    標題：移除 PostDetailView 中舊有的 tagsLabel 與相關佈局
    內容：
    - 刪除 `tagsLabel` 屬性宣告與 setupUI 中的加入與約束設定
    - 移除 bindingViewModel() 內對 `tagsLabel` 的文字設定
    - 保留其他標題、內容、按讚、反對與瀏覽數顯示邏輯
    - 為後續使用 CollectionView 重構標籤顯示做好準備

--- Views/PostView/PostDetailView.swift
+++ Views/PostView/PostDetailView.swift
@@ -23,8 +23,10 @@ class PostDetailView: UIViewController{
     
     private let titleLabel: UILabel = {
         let label = UILabel()
-        label.font = .boldSystemFont(ofSize: 22)
-        label.numberOfLines = 0
+        label.font = .boldSystemFont(ofSize: 32)
+        label.numberOfLines = 1
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.5
         return label
     }()
     
@@ -35,38 +37,23 @@ class PostDetailView: UIViewController{
         return label
     }()
     
-    private let tagsLabel: UILabel = {
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 14)
-        label.textColor = .systemBlue
-        label.numberOfLines = 1
-        return label
-    }()
-    
     private let likesLabel: UILabel = {
         let label = UILabel()
-        label.font = .systemFont(ofSize: 14)
+        label.font = .systemFont(ofSize: 12)
         label.textColor = .label
         return label
     }()
     
     private let dislikesLabel: UILabel = {
         let label = UILabel()
-        label.font = .systemFont(ofSize: 14)
+        label.font = .systemFont(ofSize: 12)
         label.textColor = .label
         return label
     }()
     
     private let viewsLabel: UILabel = {
         let label = UILabel()
-        label.font = .systemFont(ofSize: 14)
-        label.textColor = .label
-        return label
-    }()
-    
-    private let userIdLabel: UILabel = {
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 14)
+        label.font = .systemFont(ofSize: 12)
         label.textColor = .label
         return label
     }()
@@ -81,26 +68,20 @@ class PostDetailView: UIViewController{
     private func setupUI(){
         view.addSubview(titleLabel)
         view.addSubview(bodyLabel)
-        view.addSubview(tagsLabel)
         view.addSubview(likesLabel)
         view.addSubview(dislikesLabel)
         view.addSubview(viewsLabel)
-        view.addSubview(userIdLabel)
         
         titleLabel.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(16)
-            make.left.right.equalToSuperview().inset(20)
-        }
-        bodyLabel.snp.makeConstraints { make in
-            make.top.equalTo(titleLabel.snp.bottom).offset(16)
             make.left.right.equalToSuperview().inset(8)
         }
-        tagsLabel.snp.makeConstraints { make in
-            make.top.equalTo(bodyLabel.snp.bottom).offset(12)
-            make.left.right.equalToSuperview().inset(20)
+        bodyLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(12)
+            make.left.right.equalToSuperview().inset(8)
         }
         likesLabel.snp.makeConstraints { make in
-            make.top.equalTo(tagsLabel.snp.bottom).offset(12)
+            make.top.equalTo(bodyLabel.snp.bottom).offset(12)
             make.left.equalToSuperview().offset(20)
         }
         dislikesLabel.snp.makeConstraints { make in
@@ -111,10 +92,7 @@ class PostDetailView: UIViewController{
             make.top.equalTo(likesLabel.snp.bottom).offset(12)
             make.left.equalToSuperview().offset(20)
         }
-        userIdLabel.snp.makeConstraints { make in
-            make.top.equalTo(viewsLabel.snp.bottom).offset(12)
-            make.left.equalToSuperview().offset(20)
-        }
+        
     }
     
     private func bindingViewModel() {
@@ -126,14 +104,12 @@ class PostDetailView: UIViewController{
             .sink { [weak self] post in
                 self?.titleLabel.text = post.title
                 self?.bodyLabel.text = post.body
-                self?.tagsLabel.text = "Tags: " + post.tags.joined(separator: ", ")
                 self?.likesLabel.text = "Likes: \(post.reactions.likes)"
                 self?.dislikesLabel.text = "Dislikes: \(post.reactions.dislikes)"
                 self?.viewsLabel.text = "Views: \(post.views)"
-                self?.userIdLabel.text = "User ID: \(post.userId)"
+                self?.navigationItem.title = "User \(post.userId) 的文章"
             }
             .store(in: &cancellables)
     }
     
 }
-
